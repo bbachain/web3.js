a18589fa676d1fd2fb1ba99e830b56db704013a9
bbachain <bbachain123@gmail.com>
2023-03-21 10:43:06 +0200

    feat: remove test-validator

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,11 +2,6 @@
 
 set -ex
 
-# # setup environment
-# sh -c "$(curl -sSfL https://release.solana.com/edge/install)"
-# PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
-# solana --version
-
 # build and test
 npm install
 npm run build
